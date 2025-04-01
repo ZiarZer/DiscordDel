05c7b296f90c4448d3017edf909c6c967bddffa7
ZiarZer <dev@ziarzer.fr>
2025-04-01 20:58:05 +0200

    feat(client): Init React client

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components"
+
+const Wrapper =styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 1em;
+`;
+
+function App() {
+  return <Wrapper>DiscordDel</Wrapper>;
+}
+
+export default App;
